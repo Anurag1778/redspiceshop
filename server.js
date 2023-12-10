f0610c13c7547b0cb14560d73a737ee9fcf11044
champhysics@gmail.com <your_new_email@example.com>
2023-12-10 17:12:28 +0530

    deployment

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+
 import express from 'express';
 import dotenv from 'dotenv';
 import morgan from 'morgan';
@@ -6,31 +7,41 @@ import connectDB from './config/db.js';
 import cors from 'cors'
 import categoryRoutes from './routes/categoryRoutes.js'
 import productRoutes from "./routes/productRoutes.js";
+import path from 'path';
+import { fileURLToPath } from 'url';
 const app = express();
-import {fileURLToPath} from 'url'
+
 // Configure environment variables
 dotenv.config();
 
 // Connect to the database
 connectDB();
 
-const __filenmae=fileURLToPath(import.meta.url);
-const __dirname=path.dirname(__filenmae);
+// esmodule fix
+const __filename=fileURLToPath(import.meta.url)
+const __dirname=path.dirname(__filename);
+
 
 // Middleware
 app.use(cors());
 app.use(express.json());
 app.use(morgan('dev'));
-app.use(express.static(path.join(__dirname,'./client/build')))
+app.use(express.static(path.join(__dirname,"./client/build")));
+
 // Routes
 app.use('/api/v1/auth', authRoutes);
 app.use("/api/v1/category",categoryRoutes)
 app.use('/api/v1/product',productRoutes)
 
+
 // rest api
-app.use('*',function(req,res){
-    res.sendFile(path.join(__dirname,'./client/build/index.html'))
+app.use("*",function(req,res){
+    res.sendFile(path.join(__dirname,'./client/build/index.html'));
 })
+
+
+
+
 
 const PORT = process.env.PORT || 8080;
 
