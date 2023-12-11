329818d987f78b1ca372287efca22bd7f6b7410b
champhysics@gmail.com <your_new_email@example.com>
2023-12-11 07:29:03 +0530

    deployment

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
+
 import express from 'express';
 import dotenv from 'dotenv';
 import morgan from 'morgan';
 import authRoutes from './routes/authRoute.js';
 import connectDB from './config/db.js';
-import cors from 'cors';
-import categoryRoutes from './routes/categoryRoutes.js';
-import productRoutes from './routes/productRoutes.js';
+import cors from 'cors'
+import categoryRoutes from './routes/categoryRoutes.js'
+import productRoutes from "./routes/productRoutes.js";
 import path from 'path';
 import { fileURLToPath } from 'url';
-
 const app = express();
 
 // Configure environment variables
@@ -18,26 +18,30 @@ dotenv.config();
 connectDB();
 
 // esmodule fix
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const __filename=fileURLToPath(import.meta.url)
+const __dirname=path.dirname(__filename);
+
 
 // Middleware
 app.use(cors());
 app.use(express.json());
 app.use(morgan('dev'));
-
-// Serve static files
-app.use(express.static(path.join(__dirname, 'client', 'build')));
+app.use(express.static(path.join(__dirname,"./client/build")));
 
 // Routes
 app.use('/api/v1/auth', authRoutes);
-app.use('/api/v1/category', categoryRoutes);
-app.use('/api/v1/product', productRoutes);
+app.use("/api/v1/category",categoryRoutes)
+app.use('/api/v1/product',productRoutes)
 
-// REST API fallback to serve the React app
-app.get('*', function (req, res) {
-    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
-});
+
+// rest api
+app.use("*",function(req,res){
+    res.sendFile(path.join(__dirname,'/var/task/client/build/index.html'));
+})
+
+
+
+
 
 const PORT = process.env.PORT || 8080;
 
